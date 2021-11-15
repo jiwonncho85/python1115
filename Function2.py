642a6b65982d3d6cd1c2dce4dcb4cacb3437d8fd
Your Name <jiwoncho850804@gmail.com>
2021-11-15 16:24:39 +0900

    git 세번째 연습

--- Function2.py
+++ Function2.py
@@ -1,2 +1,18 @@
 # Function2.py
 
+def setValue(newValue):
+    x = newValue
+
+result = setValue(5)
+print(result)
+
+
+# 튜플 리턴
+
+def times(a,b):
+    return a+b, a*b
+
+result = times(3,4)
+print(result)
+
+
