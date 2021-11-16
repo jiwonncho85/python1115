5c16d7210e82805f4dc61b3dcef629200b735556
Your Name <jiwoncho850804@gmail.com>
2021-11-16 13:43:45 +0900

    두번째날 클래스

--- Person.py
+++ Person.py
@@ -1,16 +1,13 @@
-# class1.py
-
+# Person.py 
 class Person:
-    
-    # 생성자
     def __init__(self):
         self.name = "default name"
-        
     def print(self):
         print("My name is {0}".format(self.name))
 
+
 p1 = Person()
-p1.name = "전우치"        
 p2 = Person()
+p1.name = "전우치"
 p1.print()
 p2.print()
--- class2.py
+++ class2.py
@@ -1,11 +1,12 @@
 # class1.py
 
 class Person:
-    
+    # 클래스에 소속된 멤버 변수(데이터 공유가 목적)
+    num_person = 0
     # 생성자
     def __init__(self):
         self.name = "default name"
-        
+        Person.num_person += 1
     def print(self):
         print("My name is {0}".format(self.name))
 
@@ -14,3 +15,6 @@ p1.name = "전우치"
 p2 = Person()
 p1.print()
 p2.print()
+p3 = Person()
+
+print("인스턴스 갯수:{0}".format(Person.num_person))
