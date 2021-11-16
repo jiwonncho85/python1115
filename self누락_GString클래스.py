d421a0c68d2615712fc34b6d23f79289e1464e9b
Your Name <jiwoncho850804@gmail.com>
2021-11-16 14:54:06 +0900

    둘째날 편집

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,19 @@
+# 전역변수
 str = "Not Class Member"
+# 클래스 정의
 class GString:
+    str = ""
     def __init__(self):
+        # 인스턴스 멤버 변수
         self.str = "" 
+        GString.str = "클래스 변수"
     def set(self, msg):
         self.str = msg
     def print(self):
+        # 버그 발생
         print(str)
 
 g = GString()
 g.set("First Message")
 g.print()
+print("str:{0}".format(GString.str))
