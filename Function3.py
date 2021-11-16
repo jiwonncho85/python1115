c7651a5a1253ce8d2cf7dab246031e377dc91bc4
Your Name <jiwoncho850804@gmail.com>
2021-11-16 09:39:07 +0900

    두번째 날

--- Function3.py
+++ Function3.py
@@ -25,3 +25,25 @@ def times(a=10, b=20):
 print(times() )    
 print(times(3) )    
 print(times(3,4) )    
+
+# 키워드 인자
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+print( connectURI("naver.com", "80"))
+print( connectURI(port= "80", server= "naver.com"))
+
+# 가변인자(다양한 상황)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+print( union("HAM", "EGG"))
+print( union("HAM", "EGG", "SPAM"))
+
+
